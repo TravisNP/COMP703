02c86656ed34ccb5a7df95fe307be5390029f606
travisnp <tnpence@email.sc.edu>
2023-11-20 13:59:07 -0600

    IMP INTRO and DIS ELIM rules now record the theorem assumed in an easy way for tagging

--- COMP_PROVER/lib/Prover.ml
+++ COMP_PROVER/lib/Prover.ml
@@ -11,6 +11,12 @@ type theorem =
 
   | F (** Falsehood constructor *)
 
+let annotation_number = ref (-1)
+let next_annotation = 
+  fun () ->
+    annotation_number := !annotation_number + 1;
+    !annotation_number
+
 (** rule data type *)
 type rule = 
 
@@ -20,15 +26,15 @@ type rule =
 
   | DIS2_INTRO (** Right disjunction introduction rule *)
 
-  | IMP_INTRO (** Implication introduction rule *)
+  | IMP_INTRO of theorem (** Implication introduction rule *)
 
-  | IMP_ELIM (** Implication elimination rule *)
+  | IMP_ELIM  (** Implication elimination rule *)
 
   | CON1_ELIM (** Left conjunction elimination rule *)
 
   | CON2_ELIM (** Right conjunction elimination rule *)
 
-  | DIS_ELIM (** Disjunction elimination rule *)
+  | DIS_ELIM of theorem * theorem (** Disjunction elimination rule *)
 
   | ASSUMPTION of theorem (** Theorem is an assumpiton rule *)
 
@@ -39,12 +45,12 @@ type rule =
 (** proof data type *)
 type proof = 
 
-  | PROOF of rule * proof list * int (** Proof *)
+  PROOF of rule * proof list * int (** Rule used, the proofs of the theorems used, the success of the proof*)
 
 (** temp proof data type when keeping track of IMP and CON ELIM rules *)
 type proof_top = 
 
-  | PROOF_TOP of rule * theorem list
+  PROOF_TOP of rule * theorem list (** Rule used, the theorems used in the proof*)
 
 
 
@@ -112,13 +118,13 @@ let rec proof_to_string
         | [] -> ""
         | _ -> raise (SomethingIsWrong "proof_to_string: More than 2 children. Only zero, one, or two children possible with this implementation") in
       match rule with
-      | IMP_INTRO -> "\u{2283}I" ^ inside
+      | IMP_INTRO (_) -> "\u{2283}I" ^ inside
       | CON_INTRO -> "\u{2227}I" ^ inside
       | DIS1_INTRO -> "\u{2228}I1" ^ inside
       | DIS2_INTRO -> "\u{2228}I2" ^ inside
       | ASSUMPTION theorem -> parenthesize theorem
       | FAILURE -> "FAILURE"
-      | DIS_ELIM -> "\u{2228}E" ^ inside
+      | DIS_ELIM (_) -> "\u{2228}E" ^ inside
       | CON1_ELIM -> "\u{2227}E1" ^ inside
       | CON2_ELIM -> "\u{2227}E2" ^ inside
       | IMP_ELIM -> "\u{2283}E" ^ inside
@@ -220,7 +226,7 @@ and handle_imp_elim
 
   (* Gets the conclusions of all IMP theorems in assumptionSet where the hypothesis is in newTheoremsToAdd and the conclusion is not already an assumption *)
   let impMatchAssumptionSetConc = AssumptionSet.of_list (get_conc_if_hypo_match_list assumptionSet (AssumptionSet.to_list newTheoremsToAdd)) in
-  (* Gets the conclusions of all IMP theorems in newTHeoremsToadd where the hyptohesis is in assumptionSet and the conclusion is not already an assumption *)
+  (* Gets the conclusions of all IMP theorems in newTheoremsToadd where the hyptohesis is in assumptionSet and the conclusion is not already an assumption *)
   let impMatchTheoremsToAddConc = AssumptionSet.of_list (get_conc_if_hypo_match_list newTheoremsToAdd (AssumptionSet.to_list assumptionSet)) in
   let impMatchBothSetsConc = AssumptionSet.union impMatchAssumptionSetConc impMatchTheoremsToAddConc in
   let impMatchBothConcNotInAssumptionSet = AssumptionSet.diff impMatchBothSetsConc assumptionSet in
@@ -232,7 +238,7 @@ and handle_imp_elim
     else gen_new_assumptions assumptionSet (AssumptionSet.to_list impMatchBothConcNotInAssumptionSet) !mapRef
 
 (**  Main proof function.
-  If the theorem is an assumption, return.
+  If the theorem is an assumption, then get the shortest proof.
   Else check if theorem is of type CON, IMP, DIS, or S.
   Do INTRO rule corresponding to type CON, IMP, or DIS.
   If proof fails, do DIS ELIM rule if possible and reconsider theorem. If not possible, then fail.
@@ -285,7 +291,7 @@ let rec prover
               let proof = prover right assumptions usedDIS newMap in
               let success = is_successful proof in
               if success
-                then PROOF (IMP_INTRO, [proof], 1)
+                then PROOF (IMP_INTRO (left), [proof], 1)
                 else handle_dis_elim theorem assumptions usedDIS map
             )
         | DIS (left, right) ->
@@ -335,7 +341,7 @@ and handle_dis_elim
       let rightProof = prover theorem assumptionsRight usedDIS newRightMap in
       if (is_successful leftProof) && (is_successful rightProof)
         (* TODO: Might need to add foundDis to children for program extraction? *)
-        then PROOF (DIS_ELIM, [leftProof; rightProof], 1)
+        then PROOF (DIS_ELIM (left, right), [leftProof; rightProof], 1)
         else handle_dis_elim theorem assumptions usedDIS map
     )
   | _ -> raise (SomethingIsWrong "handle_dis_elim: DIS Theorem found in assumptions doesn't match None or DIS (left, right, false)")
@@ -351,7 +357,7 @@ and addAssumptionToMap theorem map = TheoremMap.update theorem (fun _ -> Some (P
 
 (** Calls the prover with an empty assumption set and empty usedDis set *)
 let prove_theorem
-  theorem = prover theorem AssumptionSet.empty AssumptionSet.empty TheoremMap.empty;;
+  theorem = prover theorem AssumptionSet.empty AssumptionSet.empty TheoremMap.empty
 
 (** First, prints the theorem to terminal. Then, tries to prove the theorem. Finaly, prints the proof (even upon failure) to terminal.*)
 let test_theorem
@@ -359,53 +365,53 @@ let test_theorem
   print_theorem theorem;
   let proof_theorem = prove_theorem theorem in
   print_proof proof_theorem;
-  print_newline();;
+  print_newline()
 
 (* Define operators for NOT, CON, DIS, and IMP. Precedence rules apply !! > ** > @@ > &&. All binary operators are right associative.
    Yes, it is confusing to have IMP defined as &&, however, in order to use only basic OCaml operators, which have a certain precedence order, 
    they have to be overwritten like this. https://v2.ocaml.org/manual/expr.html. Otherwise would have to download and use a dependency *)
 (** Not operator *)
 let ( !! )
-  x = IMP(x, F);;
+  x = IMP(x, F)
 
 (** Conjunction operator *)
 let ( ** )
-  x y = CON(x, y);;
+  x y = CON(x, y)
 
 (** Disjunction operator *)
 let ( @@ )
-  x y = DIS(x, y);;
+  x y = DIS(x, y)
 
 (** Implication operator *)
 let ( && ) 
-  x y = IMP(x, y);;
+  x y = IMP(x, y)
 
 (** S 1 *)
-let a = S 1;;
+let a = S 1
 
 (** S 2 *)
-let b = S 2;;
+let b = S 2
 
 (** S 3 *)
-let c = S 3;;
+let c = S 3
 
 (** S 4 *)
-let d = S 4;;
+let d = S 4
 
 (** S 5 *)
-let e = S 5;;
+let e = S 5
 
 (** S 6 *)
-let f = S 6;;
+let f = S 6
 
 (** S 7 *)
-let g = S 7;;
+let g = S 7
 
 (** S 8 *)
-let h = S 8;;
+let h = S 8
 
 (** S 9 *)
-let i = S 9;;
+let i = S 9
 
 (** S 10 *)
-let j = S 10;;
+let j = S 10
