ace3f0a83ad60ceaf76f4c4b037585ceba1e5dc1
travisnp <tnpence@email.sc.edu>
2023-12-03 23:09:33 -0600

    Comment the other examples and renamed a theorem to make it more obvious

--- COMP_PROVER/bin/testing.ml
+++ COMP_PROVER/bin/testing.ml
@@ -26,12 +26,15 @@ let functionCompositionTheorem = ((a && b) ** (b && c)) && (a && c);;
 test_theorem functionCompositionTheorem;;
 time theorem_to_proof functionCompositionTheorem;;
 
+(* Example of a theorem which takes in a function which takes in two arguments and outputs the function which takes only one argument and applies it to both 
+   arguments in the input function. For example, running this on add where add is of type (int * int) -> int results in the double function. *)
 let pairToSingleArg = ((a**a)&&b)&&(a&&b);;
 test_theorem pairToSingleArg;;
 time theorem_to_proof pairToSingleArg;;
 
-let transpose_of_2x2_or_3x3_matrix_theorem = (a**b**c)**(d**e**f)**(g**h**i) @@ (j**k)**(l**m) && (a**d**g)**(b**e**h)**(c**f**i) @@ (j**l)**(k**m);;
-test_theorem transpose_of_2x2_or_3x3_matrix_theorem;;
-time theorem_to_proof transpose_of_2x2_or_3x3_matrix_theorem;;
+(* Example of a theorem which takes in either a 3x3 or 3x3 matrix and outputes the transpose *)
+let transpose_of_3x3_or_2x2_matrix_theorem = (a**b**c)**(d**e**f)**(g**h**i) @@ (j**k)**(l**m) && (a**d**g)**(b**e**h)**(c**f**i) @@ (j**l)**(k**m);;
+test_theorem transpose_of_3x3_or_2x2_matrix_theorem;;
+time theorem_to_proof transpose_of_3x3_or_2x2_matrix_theorem;;
 let _ = ((1,(2,3)),((4,(5,6)),(7,(8,9))));;
 let _ = ((1,2),(3,4));;
